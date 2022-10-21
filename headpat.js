0aef8fcb8888860ad76208f989b14f8b1d17eee0
spock166 <21161498+spock166@users.noreply.github.com>
2022-10-20 20:18:53 -0500

    Better Pats

--- headpat.js
+++ headpat.js
@@ -3,9 +3,17 @@ var headpats=0;
 document.querySelector('#push').onclick = function(){
     var patsText = document.querySelector('#pats');
     var persyImg = document.querySelector('#persy-img');
-
     headpats+=1;
     pats.innerHTML="Headpats: "+headpats;
     persyImg.src='PersyHappy.png';
     setTimeout(()=>{persyImg.src='PersyContent.png';},250);
+}
+
+document.querySelector('#persy-img').onclick=function(){
+    var patsText = document.querySelector('#pats');
+    var persyImg = document.querySelector('#persy-img');
+    headpats+=1;
+    pats.innerHTML="Headpats: "+headpats;
+    persyImg.src='PersyHappy.png';
+    setTimeout(()=>{persyImg.src='PersyContent.png';},400);
 }
