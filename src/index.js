5e228962a96fc3387ed22ccc8b88c8f6efb5b1d0
spock166 <21161498+spock166@users.noreply.github.com>
2022-12-23 18:08:17 -0600

    Update to HashRouter

--- src/index.js
+++ src/index.js
@@ -1,9 +1,38 @@
 import React from 'react';
 import './index.css';
 import ReactDOM from 'react-dom/client';
-import KevinDoesMath from './KevinDoesMath';
+import{BrowserRouter, Routes, Route, HashRouter} from 'react-router-dom';
+
+import Home from './Pages/Home';
+import Anime from './Pages/Anime';
+import Math from './Pages/Math';
+import ToDo from './Pages/ToDo';
+import Headpat from './Pages/Headpat';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <KevinDoesMath/>
+  <HashRouter>
+  <div>
+    <div className='App-header'>
+      <a className='menu-btn' href='/'>Home</a>
+      <a className='menu-btn' href='#/anime'>Anime</a>
+      <a className='menu-btn' href='#/math'>Math</a>
+      <a className='menu-btn' href='#/todo'>To Do Utility</a>
+      <a className='menu-btn' href='#/headpat'>Headpats</a>
+
+      <a className='menu-btn' href="https://www.insidejazzkc.com/home/jazz" target="_blank">You Like Jazz?</a>
+      <a className='menu-btn' href="https://youtu.be/SCrzYRTewzU" target="_blank">You Don't Like Jazz?</a>
+    </div>
+
+    
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/anime" element={<Anime />} />
+                <Route path="/math" element={<Math />} />
+                <Route path="/todo" element={<ToDo />} />
+                <Route path="/headpat" element={<Headpat />} />
+            </Routes>
+    
+</div>
+</HashRouter>
 );
