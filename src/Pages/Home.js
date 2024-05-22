a07f320c770450811e29d0f3fda410e3024d3b31
spock166 <21161498+spock166@users.noreply.github.com>
2024-05-21 20:22:49 -0500

    Add new nerds

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,24 +1,44 @@
 import '../App.css';
 
-function Home() {  
-  
+function Home() {
   return (
     <div>
       <div className='content-container'>
         <h1>Who is Kevin?</h1>
         <p>
-        I received a B.S. in mathematics in 2016 from Northwest Missouri State University (Go Bearcats!).  In 2022 I finished my Ph.D. in mathematics at the University of Kansas where I worked with <a href="https://jlmartin.ku.edu/"> Jeremy Martin</a> and I studied algebraic combinatorics.
+          I received a B.S. in mathematics in 2016 from Northwest Missouri State University (Go Bearcats!).  In 2022 I finished my Ph.D. in mathematics at the University of Kansas where I worked with <a href="https://jlmartin.ku.edu/"> Jeremy Martin</a> and I studied algebraic combinatorics.
         </p>
       </div>
-      
+
       <div className='content-container'>
-        <h1>Fun Things</h1>
+        <h1>Nerds Who Nerd</h1>
         <ul>
-          <li><a href="https://sandyuraz.com/">Sandy's Awesome Page</a></li>
+          {FriendList()}
         </ul>
       </div>
     </div>
   );
 }
+
+function FriendList() {
+  const friends = {
+    "Sandy": "https://sandyuraz.com/",
+    "Jimmy": "https://jameshurd.net/",
+    "Josie": "https://jswalz.github.io/",
+  }
+  const friendsList = Object.keys(friends).map(friend => {
+    return <li><a href={friends[friend]} target="_blank">{friend}</a></li>
+  })
+
+  return shuffleList(friendsList)
+}
+
+function shuffleList(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
 
 export default Home;
